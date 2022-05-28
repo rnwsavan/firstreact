2cbf583321d0f83f8ca57908911f97204eb58563
rnwsavan <rnwsavanasodariya@gmail.com>
2022-05-28 19:41:07 +0530

    Form change

--- src/container/Home/Contact.js
+++ src/container/Home/Contact.js
@@ -1,6 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import * as yup from 'yup';
+import { Form, Formik, useFormik } from 'formik';
+import { Button, Input } from 'reactstrap';
 
 function Contact(props) {
+    const [userform, setuserform] = useState("Form_1");
+
+    let Form_1 = {
+        name: yup.string().required("please enter your name"),
+        email: yup.string().email("please enter valid email").required("please enter email"),
+        subject: yup.string().required("please enter your subject"),
+    }
+
+    let schema, initialVal;
+
+    schema = yup.object().shape(Form_1);
+    initialVal = {
+        name: "",
+        email: "",
+        subject: ""
+    }
+
+    const formik = useFormik({
+        initialValues: initialVal,
+        validationSchema: schema,
+        onSubmit: (values, { resetForm }) => {
+            if (userform === "Form_1") {
+                console.log("Successfully Send Message");
+            }
+            resetForm()
+        },
+    });
+    console.log(formik.errors.name);
+    console.log(formik.errors.email);
+    console.log(formik.errors.subject);
     return (
         <main>
             <section id="contact" className="contact">
@@ -34,28 +67,43 @@ function Contact(props) {
                             </div>
                         </div>
                         <div className="col-lg-8 mt-5 mt-lg-0">
-                            <form action method="post" role="form" className="php-email-form">
-                                <div className="row">
-                                    <div className="col-md-6 form-group">
-                                        <input type="text" name="name" className="form-control" id="name" placeholder="Your Name" required />
+                            <Formik value={formik}>
+                                <Form onSubmit={formik.handleSubmit}>
+                                    <div className="row">
+                                        <div className="col-md-6 form-group">
+                                            <Input type="text" name="name" className="form-control" id="name" placeholder="Your Name" onChange={formik.handleChange} />
+                                            {
+                                                formik.errors.name ?
+                                                <p>{formik.errors.name}</p> : null
+                                            }
+                                        </div>
+                                        <div className="col-md-6 form-group mt-3 mt-md-0">
+                                            <Input type="email" className="form-control" name="email" id="email" placeholder="Your Email" onChange={formik.handleChange} />
+                                            {
+                                                formik.errors.email ? 
+                                                <p>{formik.errors.email}</p> : null
+                                            }
+                                        </div>
                                     </div>
-                                    <div className="col-md-6 form-group mt-3 mt-md-0">
-                                        <input type="email" className="form-control" name="email" id="email" placeholder="Your Email" required />
+                                    <div className="form-group mt-3">
+                                        <Input type="text" className="form-control" name="subject" id="subject" placeholder="Subject" onChange={formik.handleChange} />
+                                        {
+                                                formik.errors.subject ? 
+                                                <p>{formik.errors.subject}</p> : null
+                                            }
                                     </div>
-                                </div>
-                                <div className="form-group mt-3">
-                                    <input type="text" className="form-control" name="subject" id="subject" placeholder="Subject" required />
-                                </div>
-                                <div className="form-group mt-3">
-                                    <textarea className="form-control" name="message" rows={5} placeholder="Message" required defaultValue={""} />
-                                </div>
-                                <div className="my-3">
-                                    <div className="loading">Loading</div>
-                                    <div className="error-message" />
-                                    <div className="sent-message">Your message has been sent. Thank you!</div>
-                                </div>
-                                <div className="text-center"><button type="submit">Send Message</button></div>
-                            </form>
+                                    <div className="form-group mt-3">
+                                        <textarea className="form-control" name="message" rows={5} placeholder="Message" onChange={formik.handleChange} defaultValue={""} />
+                                        
+                                    </div>
+                                    <div className="my-3">
+                                        <div className="loading">Loading</div>
+                                        <div className="error-message" />
+                                        <div className="sent-message">Your message has been sent. Thank you!</div>
+                                    </div>
+                                    <div className="text-center"><Button type="submit" onClick={() => setuserform("Form_1")}>Send Message</Button></div>
+                                </Form>
+                            </Formik>
                         </div>
                     </div>
                 </div>
